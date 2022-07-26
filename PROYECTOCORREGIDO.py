6347cb18ecbd800c981c624ccc788d3c581ad3f8
fvexe82 <106171748+fvexe82@users.noreply.github.com>
2022-07-26 20:44:25 -0300

    Add files via upload

---
+++ PROYECTOCORREGIDO.py
@@ -0,0 +1,42 @@
+print("=============================================")
+print("Bienvenidos!.... A continuacion, debera Ingresar 5 Numeros Enteros")
+print("=============================================", "\n")
+   
+num = 5
+lista = []
+suma = 0 
+        
+for i in range ( 1, 6):
+    valor = int(input(f"Escriba el Numero Entero {i}: "))
+    lista  = lista + [valor]
+    suma = suma + i
+def funcion_sumar ():
+    
+    print("=============================================", "\n")    
+    print("La Suma de Numeros Enteros es Igual a: ", suma)
+
+funcion_sumar()
+
+def funcion_promedio():        
+    promedio = 0
+    promedio = suma / num
+    print("=============================================", "\n")
+    print("El Promedio es: " ,promedio)
+    print("=============================================", "\n")
+
+funcion_promedio()
+
+        
+print("La Lista contiene los Siguientes Valores: ", lista)
+print("=============================================", "\n")
+
+def funcion_maximo ():
+    print("El Valor maximo ingresado es: ",max(lista))
+    print("=============================================", "\n")
+
+funcion_maximo ()
+        
+
+
+              
+        
